baf7bc9a8d5b9468b8e17e31d262147361b61ff3
Lea Aliperta <leacma2244@gmail.com>
2019-04-27 20:18:14 +0100

    Update exercises.js
    fibonacci bu only on even numbers

--- exercises.js
+++ exercises.js
@@ -5,8 +5,10 @@ var myarray = [];
   myarray[i] = i;
   }
   for( var i = 1; i < 100; i++ ){
+    if( i % 2 == 0) {
   myarray[i] = myarray[i-1] + myarray[i];
   }
+}
   for( var i = 0; i < 100; i++ ){
   console.log("Value of i: " + myarray[i]);
   }
